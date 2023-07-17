4802fb89f2ac8057ea07b0ec70b3b3166685a403
Argiro <elto.82@gmai.com>
2023-07-16 20:45:57 -0500

    creacion de proyecto

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="container">
+      <div className="jumbotron">
+        <p className="lead text-center">Buscador de Imágenes</p>
+      </div>
+    </div>
+  );
+};
+
+export default App;
