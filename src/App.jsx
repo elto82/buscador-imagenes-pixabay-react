549142d89443c30512dbbec95c79f93c6f20c14c
Argiro <elto.82@gmai.com>
2023-07-17 12:48:59 -0500

    paginacion

--- src/App.jsx
+++ src/App.jsx
@@ -14,15 +14,28 @@ const App = () => {
 
       const imagenePorPagina = 30;
       const key = "33399484-e1d7cbba2d2152a34154a5962";
-      const url = `https://pixabay.com/api/?key=${key}&q=${busqueda}&per_page=${imagenePorPagina}`;
+      const url = `https://pixabay.com/api/?key=${key}&q=${busqueda}&per_page=${imagenePorPagina}&page=${paginaActual}`;
       const respuesta = await fetch(url);
       const resultado = await respuesta.json();
       setImagenes(resultado.hits);
 
       setTotalPaginas(Math.ceil(resultado.totalHits / imagenePorPagina));
+
+      const jumbotron = document.querySelector(".jumbotron");
+      jumbotron.scrollIntoView({ behavior: "smooth" });
     };
     consultarApi();
-  }, [busqueda]);
+  }, [busqueda, paginaActual]);
+
+  const handlePaginaAnterior = () => {
+    if (paginaActual === 0) return;
+    setPaginaActual(paginaActual - 1);
+  };
+
+  const handlePaginaSiguiente = () => {
+    if (paginaActual > totalPaginas) return;
+    setPaginaActual(paginaActual + 1);
+  };
 
   return (
     <div className="container">
@@ -30,8 +43,27 @@ const App = () => {
         <p className="lead text-center">Buscador de Imágenes</p>
         <Formulario setBusqueda={setBusqueda} />
       </div>
-      <div className="row justify-content-center">
+      <div className="row justify-content-center mb-4">
         <ListadoImagenes imagenes={imagenes} />
+        {paginaActual === 1 ? null : (
+          <button
+            type="button"
+            className="btn btn-info mr-1 "
+            onClick={handlePaginaAnterior}
+          >
+            Anterior &laquo;
+          </button>
+        )}
+
+        {paginaActual === totalPaginas ? null : (
+          <button
+            type="button"
+            className="btn btn-info ml-4"
+            onClick={handlePaginaSiguiente}
+          >
+            Siguiente &raquo;
+          </button>
+        )}
       </div>
     </div>
   );
